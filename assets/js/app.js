2ffe3cc8aed93228d0963716ff0851c612a137cd
Bayramzade066 <bayramzade.313@gmail.com>
2021-12-06 14:27:36 +0400

    update

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+var myswiper = new Swiper(".mySwiper", {
+    
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
