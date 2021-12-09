8f73321c9e4d17c90bf87c29b3e09e808df8fec0
Rovshen98 <rovshens@bk.ru>
2021-12-09 11:47:52 +0400

    box

--- pragmatech design/assets/js/app.js
+++ pragmatech design/assets/js/app.js
@@ -10,3 +10,18 @@ var myswiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  // search
+  $(".btn").click(function () {
+    $(".input").toggleClass("active").focus;
+    $(this).toggleClass("animate");
+    $(".input").val("");
+    $(".menu ul").toggleClass("menu_ul_active")
+    $(".btn1").toggleClass("btn1-animate")
+    
+    
+  });
+
+  
+  
