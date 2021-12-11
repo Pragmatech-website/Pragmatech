41169992b1203a1238f162d901e2d632bcb4fcf0
Rovshen98 <rovshens@bk.ru>
2021-12-11 17:25:22 +0400

    push

--- pragmatech design/assets/js/app.js
+++ pragmatech design/assets/js/app.js
@@ -12,6 +12,57 @@ var myswiper = new Swiper(".mySwiper", {
   });
 
 
+// navbarscroll
+var header = document.querySelector("header");
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  
+  
+      var currentScrollPos = window.pageYOffset;
+      
+      
+     
+
+       if (prevScrollpos > currentScrollPos) {
+      header.style.top = "0";
+      
+      header.style.transition = "0.5s";
+      if(window.pageYOffset < 130){
+        
+        header.style.backgroundColor="unset"
+        header.style.position="fixed"
+       
+      }
+        
+    
+    }
+    
+    
+    
+    else {
+    header.style.top = "-6.625rem";
+    header.style.position= "fixed";
+    header.style.backgroundColor= "#333";
+    if(window.pageYOffset < 130){
+      
+      header.style.backgroundColor="unset"
+      header.style.position="fixed"
+     
+    }
+   
+    
+    }
+     prevScrollpos = currentScrollPos;
+    
+
+}
+
+
+
+
+
+
   // search
   $(".btn").click(function () {
     $(".input").toggleClass("active").focus;
