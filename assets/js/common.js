d374cf898228e34053fe422c267b342aabe08b0e
Bayramzade066 <bayramzade.313@gmail.com>
2021-12-13 05:05:13 +0400

    MissionComplete

---
+++ assets/js/common.js
@@ -0,0 +1,61 @@
+// navbarscroll
+var header = document.querySelector("header");
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  
+            
+      var currentScrollPos = window.pageYOffset;
+      
+      
+     
+
+       if (prevScrollpos > currentScrollPos) {
+      header.style.top = "0";
+      
+      header.style.transition = "0.5s";
+      if(window.pageYOffset < 130){
+        
+        header.style.backgroundColor="#F5F5F5"
+        header.style.position="fixed"
+       
+      }
+        
+    
+    }
+    
+    
+    
+    else {
+    header.style.top = "-6.625rem";
+    header.style.position= "fixed";
+    header.style.backgroundColor= "#F5F5F5";
+    if(window.pageYOffset < 130){
+      
+      header.style.backgroundColor="#F5F5F5"
+      header.style.position="fixed"
+     
+    }
+   
+    
+    }
+     prevScrollpos = currentScrollPos;
+    
+
+}
+
+
+
+
+
+
+  // search
+  $(".btn").click(function () {
+    $(".input").toggleClass("active").focus;
+    $(this).toggleClass("animate");
+    $(".input").val("");
+    $(".menu ul").toggleClass("menu_ul_active")
+    $(".btn1").toggleClass("btn1-animate")
+    
+    
+  });
